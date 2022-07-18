54161ce0404a159b0b70931c90d835b34e239ce7
Marc Murray <mmarc.96@gmail.com>
2022-07-18 12:59:10 +0100

    Created Intake and Simulator classes

---
+++ src/main/java/com/sparta/simulator/Intake.java
@@ -0,0 +1,59 @@
+package com.sparta.simulator;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+public class Intake {
+	Collection trainingCentres = new ArrayList();
+
+	private int waitingList = 0;
+
+	public int getWaitingList() {
+		return waitingList;
+	}
+
+	public void addCentre(){
+		TrainingCentre.add(new TrainingCentre());
+	}
+	public int getTrainees(){
+		int sum = 0;
+		for (TrainingCentre centre: trainingCentres){
+			sum += centre.getTraineeCount();
+		}
+		return sum;
+	}
+
+	public int getFullCentresNum(){
+		int sum = 0;
+		for (TrainingCerntre centre: trainingCentres){
+			if (centre.isFull()){
+				sum ++;
+			}
+		}
+		return sum;
+	}
+
+	public int openCentres(){
+		return trainingCentres.size();
+	}
+	public int traineesInTraining(){
+		int sum = 0;
+		for (TrainingCerntre centre: trainingCentres){
+			sum +=centre.getTraineeCount();
+		}
+		return sum;
+	}
+
+
+	public void addTraineeGroup(){
+		for (TrainingCentre centre: trainingCentres){
+			int randomNum = new Random().nextInt(50);
+			if (randomNum<= waitingList){
+				centre.addTrainees(randomNum);
+				waitingList -= randomNum;
+			}
+		}
+	}
+}
---
+++ src/main/java/com/sparta/simulator/Simulator.java
@@ -0,0 +1,32 @@
+package com.sparta.simulator;
+
+public class Simulator {
+	private Intake intake;
+
+	private final int CENTRE_GENERATION_INTERVAL = 2; // centre generation interval (months) times 4 we are working in weeks, 4 weeks in a month,
+	// about the
+	private final int totalDuration; // Total duration of the simulation (months).
+
+	public Simulator(int totalDuration){
+		this.intake = new Intake();
+		this.totalDuration = totalDuration;
+	}
+	//Will probably return some results back to the controller so may not be void.
+	public void tick(){
+		//Generate new Trainees through intake.
+		intake.addTraineeGroup();
+		
+	}
+
+	public void run(){
+		for (int i = 0; i< totalDuration; i++){
+			if(i % CENTRE_GENERATION_INTERVAL == 0){
+				//Generate new TrainingCentre through Intake then tick.
+				intake.addCentre();
+			}
+			tick();
+		}
+	}
+
+
+}
