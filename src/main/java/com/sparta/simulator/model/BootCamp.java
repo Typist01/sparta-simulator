cc968df285b6a508afbb0c68d063ddf20e0382af
Marc Murray <mmarc.96@gmail.com>
2022-07-19 12:17:28 +0100

    Make Bootcamp class

---
+++ src/main/java/com/sparta/simulator/model/BootCamp.java
@@ -0,0 +1,17 @@
+package com.sparta.simulator.model;
+
+public class BootCamp extends Centre {
+	private final int CAPACITY = 500;
+	@Override
+	void addTrainee() {
+
+	}
+
+	@Override
+	boolean isFull() {
+		return super.;
+	}
+
+
+
+}
