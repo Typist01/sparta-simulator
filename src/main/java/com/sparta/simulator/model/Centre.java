1af81e511068b80ef059daf94d061225c5f950b5
Jamie Scofield <jamies_1048@outlook.com>
2022-07-19 12:11:24 +0100

    made abstract class+

--- src/main/java/com/sparta/simulator/model/Centre.java
+++ src/main/java/com/sparta/simulator/model/Centre.java
@@ -2,56 +2,19 @@ package com.sparta.simulator.model;
 
 import java.util.ArrayList;
 
-public class Centre {
+public abstract class Centre {
     ArrayList<Trainee> traineeList = new ArrayList();
-//    private int extraTrainees = 0;
 
     Centre(){
-        //trainees = new ArrayList<Trainee>[];
-    }
-    /*void addTrainees(int numOfTrainees) { // insert random num between 0 and 50
-        for (int i = 0; i < numOfTrainees; i++) {
-            if (!this.isFull()) {    //add trainees if centre not full
-                traineeList.add(new Trainee());
-            } else {
-                extraTrainees = (numOfTrainees - i); //stops loop and correctly sets num of extras
-                break;
-            }
-        }
-    }*/
-
-    /*void addTrainees(int numOfTrainees, ArrayList<Trainee> inputList) { // don't put in more than it can handle
-
-        for (int i = 0; i < numOfTrainees; i++) {
-            if (!this.isFull()) {    //add trainees if centre not full
-                traineeList.add(inputList.get(i));
-                inputList.remove(i);
-                //traineeList.add(new Trainee());
-            } else {
-                //extraTrainees = (numOfTrainees - i); //stops loop and correctly sets num of extras
-                break;
-            }
-        }
-    }*/
-
-    void addTrainee(Trainee trainee){
-        traineeList.add(trainee);
     }
 
-    public int getTraineeCount(){
-        return traineeList.size();
-    }
-
-//    int getExtraTrainees(){
-//        return extraTrainees;
-//    }
+    abstract void addTrainee();
 
     int getNumOfTrainees(){
         return traineeList.size();
     }
 
-    boolean isFull(){
-        return traineeList.size() >= 100 ;
-    }
+   abstract boolean isFull();
+
 
 }
