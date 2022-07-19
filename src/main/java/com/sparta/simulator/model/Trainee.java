0d9b434cfc37bedcaa172f0e74e24272d9be8312
mustafa <travellingtypist1@gmail.com>
2022-07-19 14:25:08 +0100

    Trainee constructor complete

--- src/main/java/com/sparta/simulator/model/Trainee.java
+++ src/main/java/com/sparta/simulator/model/Trainee.java
@@ -1,11 +1,39 @@
 package com.sparta.simulator.model;
 
+import java.util.Random;
+
 public class Trainee {
+    private String courseType;
 
-
-    String type;
-
-    public String getType() {
-        return type;
+    public Trainee(){
+        int randomNum = new Random().nextInt(5);
+        
+        if (randomNum == 0)
+        {
+            courseType = "Java";
+        }
+        else if (randomNum == 1)
+        {
+            courseType = "C#";
+        }
+        else if(randomNum == 2)
+        {
+            courseType = "Data";
+        }
+        else if(randomNum == 3)
+        {
+            courseType = "DevOps";
+        }
+        else if(randomNum == 4)
+        {
+            courseType = "Business";
+        }
     }
+//    Trainee(String courseType){
+//        courseType = courseType
+//    }
+    public String getType() {
+        return courseType;
+    }
+
 }
