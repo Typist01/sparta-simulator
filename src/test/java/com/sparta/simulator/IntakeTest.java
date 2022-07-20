f713dfe75d0db09c916b51198e557b1543cae33b
mustafa <travellingtypist1@gmail.com>
2022-07-20 14:33:24 +0100

    fixed centre generation method, added max limit of bootcamps (2)

--- src/test/java/com/sparta/simulator/IntakeTest.java
+++ src/test/java/com/sparta/simulator/IntakeTest.java
@@ -1,19 +1,39 @@
 package com.sparta.simulator;
 
 import com.sparta.simulator.model.Intake;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntakeTest {
 
-    @Test
-    void testWaitingListGeneration(){
-        for (int i = 0; i < 100; i++) {
-            Intake intake = new Intake();
-            intake.addTraineeGroup();
-            assertTrue(intake.getWaitingCount() >= 50 && intake.getWaitingCount()<=100);
-        }
+    Intake intake;
+    @BeforeEach
+    void Initialise(){
+        intake = new Intake();
     }
+//    @Test
+//    void testWaitingListGeneration(){
+//        for (int i = 0; i < 100; i++) {
+//            Intake intake = new Intake();
+//            intake.addTraineeGroup();
+//            assertTrue(intake.getWaitingCount() >= 50 && intake.getWaitingCount()<=100);
+//        }
+//    }
+
+    @Test
+    void testGenerateCentre(){
+        System.out.println(intake.getTrainingCentres());
+        intake.createCentresRandomly();
+        System.out.println(intake.getTrainingCentres());
+
+    }
+
+
+
+
 
 }
