5b7d2ffdfda77c49ca48d3174673d449c137be1e
Marc Murray <mmarc.96@gmail.com>
2022-07-20 15:29:39 +0100

    Fix issue with traininghubs not accepting trainees

--- src/main/java/com/sparta/simulator/model/TrainingHub.java
+++ src/main/java/com/sparta/simulator/model/TrainingHub.java
@@ -3,16 +3,8 @@ package com.sparta.simulator.model;
 import java.util.ArrayList;
 
 public class TrainingHub extends Centre{
-//can train a maximum of 100 trainees, but 3 (randomly 1-3) can be opened at a time each month.
-    private final ArrayList<Trainee> list = getTraineeList();
-
     protected TrainingHub() {
         super(100);
-    }
-
-    @Override
-    boolean acceptsTrainee(Trainee trainee) {
-        return false;
     }
 
 }
